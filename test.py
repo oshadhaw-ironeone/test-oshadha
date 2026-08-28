3fe18df18086186da876b1b8d67ace73115df741
Oshadha Wijerathne <rwkoshadhawijerathne@gmail.com>
2026-08-28 21:03:59 +0530

    s

--- test.py
+++ test.py
@@ -1,7 +1,40 @@
-sample_id = matched_rows["Authorization Identifier"].iloc[0]
+def clean_id(series):
+    return pd.to_numeric(series, errors="coerce").astype("Int64").astype("string").str.strip()
 
-print("df_y row:")
-print(df_y[df_y["Authorization Identifier"] == sample_id][["Authorization Identifier", "Merchant Name", "Authorization Amount"]])
 
-print("\ndf row:")
-print(df[df["Authorization Identifier"] == sample_id][["Authorization Identifier", "Merchant Name", "Transaction Amount"]])
+def create_transaction_match_key_confirm_fraud(df):
+    cols = [
+        "Authorization Identifier",
+        "Merchant Name",
+        "Transaction Amount"
+    ]
+
+    mask = df[cols].notna().all(axis=1)
+
+    df["transaction_match_key"] = pd.NA
+
+    df.loc[mask, "transaction_match_key"] = (
+        clean_id(df.loc[mask, "Authorization Identifier"]) + "|" +
+        df.loc[mask, "Merchant Name"].astype("string").str.strip() + "|" +
+        df.loc[mask, "Transaction Amount"].astype("string").str.strip()
+    )
+    return df
+
+
+def create_transaction_match_key_fraud_analysis(df):
+    cols = [
+        "Authorization Identifier",
+        "Merchant Name",
+        "Authorization Amount"
+    ]
+
+    mask = df[cols].notna().all(axis=1)
+
+    df["transaction_match_key"] = pd.NA
+
+    df.loc[mask, "transaction_match_key"] = (
+        clean_id(df.loc[mask, "Authorization Identifier"]) + "|" +
+        df.loc[mask, "Merchant Name"].astype("string").str.strip() + "|" +
+        df.loc[mask, "Authorization Amount"].astype("string").str.strip()
+    )
+    return df
